993e74017d73720304eb7db315313ef531a5c913
Shubham Modi <shubham14modi@gmail.com>
2026-01-04 13:05:13 +0530

    Props in class vs functions component

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,42 @@
 import User from "./User";
 import UserClass from "./UserClass";
+import React from "react";
 
-const About = () => {
-  return (
-    <div>
-      <h1>About</h1>
-      <h2>This is about page of dummy Food point APP.</h2>
-      <User name={"Shubham Modi functional"} location={"Pune, Maharastra"} />
-      <UserClass name={"Shubham Modi class"} location={"Dhanbad, Jharkhand"} />
-    </div>
-  );
-};
+// const About = () => {
+//   return (
+//     <div>
+//       <h1>About</h1>
+//       <h2>This is about page of dummy Food point APP.</h2>
+//       <User name={"Shubham Modi functional"} location={"Pune, Maharastra"} />
+//       <UserClass name={"Shubham Modi class"} location={"Dhanbad, Jharkhand"} />
+//     </div>
+//   );
+// };
+
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("Parent Constructor");
+  }
+
+  componentDidMount() {
+    console.log("Parent component Did Mount");
+  }
+
+  render() {
+    console.log("Parent render");
+    return (
+      <div>
+        <h1>About</h1>
+        <h2>This is about page of dummy Food point APP.</h2>
+        <User name={"Shubham Modi functional"} location={"Pune, Maharastra"} />
+        <UserClass name={"First child"} location={"Dhanbad, Jharkhand"} />
+        <UserClass name={"Second child"} location={"Dhanbad, Jharkhand"} />
+        <UserClass name={"Third child"} location={"Dhanbad, Jharkhand"} />
+        <UserClass name={"Forth child"} location={"Dhanbad, Jharkhand"} />
+      </div>
+    );
+  }
+}
 
 export default About;
--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 const User = ({ name, location }) => {
   //const { name, location } = props;
+  const [count, setCount] = useState(0);
+  const [count1, setCount1] = useState(1);
+
   return (
     <div className="user-card">
+      <h2>Count: {count}</h2>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Add Count
+      </button>
       <h3>Name: {name}</h3>
       <h3>Location: {location}</h3>
       <h4>Contact: shubham@gamil.com</h4>
